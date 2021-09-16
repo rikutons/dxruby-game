c965c32a8e3175990eb10fd7836665239658f4d1
rikutons <rikutonsicloud@gmail.com>
2021-09-16 13:38:36 +0900

    wip

--- main.rb
+++ main.rb
@@ -2,6 +2,11 @@ require 'dxruby'
 
 sprites = []
 player = Sprite.new(0, 0, Image.new(10, 10).circle_fill(0, 0, 10, [50, 50, 255]))
+sprites << player
+start_font = Font.new(24)
+while Input.key_push?(K_SPACE)
+  Window.draw_font()
 Window.loop do
-  player.draw
+  Sprite.update(sprites)
+  Sprite.draw(sprites)
 end
