4cf10c691e1f08137f1bbff90930f48bb23424e5
rikutons <rikutonsicloud@gmail.com>
2021-09-16 13:25:08 +0900

    @aa

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 require 'dxruby'
 
 sprites = []
-player = Sprite.new(0, 0, Image.circle_fill(0, 0, 10, [0, 255, 0]))
+player = Sprite.new(0, 0, Image.new(10, 10).circle_fill(0, 0, 10, [50, 50, 255]))
 Window.loop do
   player.draw
 end
