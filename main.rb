cdb1efc8ecfa225bf576177c67578cc17fa9fb22
RIkuto Sueta <sueta@Lenovo-Y520-15IKBN-a4d8866b.localdomain>
2021-09-16 13:18:32 +0900

    a

--- main.rb
+++ main.rb
@@ -1,22 +1,7 @@
 require 'dxruby'
 
-require_relative 'player'
-require_relative 'enemy'
-
-player_img = Image.load("image/player.png")
-enemy_img = Image.load("image/enemy.png")
-
-player = Player.new(100, 100, player_img) # 簡潔にする
-enemies = [] # 書き換え、以下追加
-10.times do
-  enemies << Enemy.new(rand(0..(640 - 32 - 1)), rand((480 - 32 - 1)), enemy_img)
-end
-
+sprites = []
+player = Sprite.new(0, 0, Image.circle_fill(0, 0, 10, [0, 255, 0]))
 Window.loop do
-  player.update
   player.draw
-
-  Sprite.draw(enemies)
-
-  Sprite.check(player, enemies) # 書き換え
 end
